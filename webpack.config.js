bcc8d5573921ae106075336131b4cc567297d149
Arif Shaikh <arifsbs@yahoo.com>
2020-04-25 21:28:49 +0100

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     //entry: "./src/playground/redux-expensify.js",
     // entry: "./src/playground/redux-expensify.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
     },
     module: {
@@ -46,6 +46,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      publicPath: "/dist/",
     },
   };
 };
